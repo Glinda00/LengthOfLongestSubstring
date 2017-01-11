4faf184eae7416662d3b8ca8c4be99197c05c081
Administrator <Administrator@PC201604141706>
2017-01-11 15:56:34 +0800

    第一次提交，可进行各类字符串输入测试

---
+++ src/LengthOfLongestSubstring.java
@@ -0,0 +1,38 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class LengthOfLongestSubstring {  	  
+    public static int lengthOfLongestSubstring(String str) { 
+    	if(str == null || str.length() == 0){
+    		return 0;
+    	}
+        Map<Character,Integer> map = new HashMap<Character,Integer>();  
+        int maxLength = 0;  
+        int now = 0;  
+        int length = str.length();
+        for(int i = 0;i < length;i++){    
+            if(map.containsKey(str.charAt(i))){   
+                now = Math.max(map.get(str.charAt(i)) + 1,now);  
+                if((i - now + 1) > maxLength){  
+                    maxLength = i - now + 1;         
+                }  
+            }else{  
+                if((i - now + 1) > maxLength){  
+                    maxLength = i - now + 1;  
+                }  
+            }  
+            map.put(str.charAt(i), i);  
+        }  
+        return maxLength;  
+    }  
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		while (sc.hasNext()){
+			String str = sc.nextLine();
+			System.out.println(lengthOfLongestSubstring(str));
+		}
+		sc.close();
+	}
+}
